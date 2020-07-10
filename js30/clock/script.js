ce36119ee8726a0306f70b38b289cec9e47d1965
ericmahoney <eric.mahoney22@gmail.com>
2020-07-10 16:58:35 -0400

    added favicons

---
+++ js30/clock/script.js
@@ -0,0 +1,21 @@
+function getTime() {
+  return new Date();
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+function setTime() {
+  document.getElementById("full-time").innerHTML = getTime();
+  document.getElementById("hour-min-time").innerHTML = formatAMPM(getTime());
+}
+
+setInterval(setTime, 1000);
