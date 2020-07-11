fefc2d16a84636eb23e240346759bb923b2d30ea
ericmahoney <eric.mahoney22@gmail.com>
2020-07-11 00:06:02 -0400

    added clock project to js30

--- js30/clock/script.js
+++ js30/clock/script.js
@@ -16,6 +16,21 @@ function formatAMPM(date) {
 function setTime() {
   document.getElementById("full-time").innerHTML = getTime();
   document.getElementById("hour-min-time").innerHTML = formatAMPM(getTime());
+  setAnalog(getTime());
+}
+
+function setAnalog(date) {
+  var seconds = date.getSeconds();
+  var minutes = date.getMinutes();
+  var hours = date.getHours();
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  hours = (hours / 12) * 360 - 180;
+  minutes = (minutes / 60) * 360 - 180;
+  seconds = (seconds / 60) * 360 - 180;
+  document.getElementById("hour-hand").style.transform = `rotate(${hours}deg)`;
+  document.getElementById("minute-hand").style.transform = `rotate(${minutes}deg)`;
+  document.getElementById("second-hand").style.transform = `rotate(${seconds}deg)`;
 }
 
 setInterval(setTime, 1000);
