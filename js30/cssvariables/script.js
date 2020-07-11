1e5b8a48bdefd5f319063d67f44ae7cc97846f06
ericmahoney <eric.mahoney22@gmail.com>
2020-07-11 19:33:30 -0400

    fixed naming of sliders to be more correct

--- js30/cssvariables/script.js
+++ js30/cssvariables/script.js
@@ -1,13 +1,21 @@
 function updateBoxShadow() {
-  const xAxis = document.getElementById("xAxisSlider").value;
-  const yAxis = document.getElementById("yAxisSlider").value;
+  const xOffset = document.getElementById("xOffsetSlider").value;
+  const yOffset = document.getElementById("yOffsetSlider").value;
   const blur = document.getElementById("blurSlider").value;
   const color = document.getElementById("colorSlider").value;
   const card = document.getElementById("sample-card");
   const output = document.getElementById("box-shadow-output");
-  const css = `${xAxis}px ${yAxis}px ${blur}px ${color}`;
+  const css = `${xOffset}px ${yOffset}px ${blur}px ${color}`;
+
+  console.log(css);
 
   card.style.boxShadow = css;
 
   output.innerHTML = "box-shadow: " + css + ";";
 }
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => input.addEventListener("change", updateBoxShadow));
+inputs.forEach((input) => input.addEventListener("mousemove", updateBoxShadow));
+document.getElementById("colorSlider").addEventListener("mousedown", updateBoxShadow);
