62805c1df3f295a4888f211a3643b2ec6ac45983
ericmahoney <eric.mahoney22@gmail.com>
2020-10-24 12:48:37 -0400

    added dice simluator

---
+++ dice/script.js
@@ -0,0 +1,20 @@
+function simDice() {
+  return Math.ceil(Math.random() * 6);
+}
+
+function generateDice() {
+  const num = document.getElementById("dice-num").value;
+  const container = document.getElementById("dice-container");
+  container.innerHTML = "";
+  for (var i = 0; i < num; i++) {
+    let newDice = document.createElement("div");
+    let num = simDice();
+    newDice.innerHTML = `<img class="dice-img" src="/dice-images/${num}.png">`;
+    newDice.classList.add("dice-roll");
+    container.appendChild(newDice);
+  }
+}
+
+document.getElementById("dice-num").addEventListener("change", () => {
+  generateDice();
+});
